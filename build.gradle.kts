a1961bccf28d66e70134f50ad1ec0efb7274eb08
LucisUrbe <pm.lu@pm.me>
2024-04-22 15:39:22 +0800

    feat: ignore hyphen in the tag to establish the same version string during build

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,7 +58,7 @@ cmaker {
 
 val repo = jgit.repo()
 val commitCount = (repo?.commitCount("refs/remotes/origin/master") ?: 1) + 4200
-val latestTag = repo?.latestTag?.removePrefix("v") ?: "1.0"
+val latestTag = repo?.latestTag?.removePrefix("v")?.substringBefore("-") ?: "1.0"
 
 val injectedPackageName by extra("com.android.shell")
 val injectedPackageUid by extra(2000)
