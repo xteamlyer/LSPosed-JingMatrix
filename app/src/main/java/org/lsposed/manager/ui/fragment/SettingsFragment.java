197ba7360df13f02c5f6942654c19cc19f7e7ee7
Zero Re <140869597+re-zero001@users.noreply.github.com>
2024-09-06 16:09:37 +0800

    Revert "remove add shortcut setting"
    This reverts commit c7e5fd9fa1604a4245b0b2361deb27364352d142.

--- app/src/main/java/org/lsposed/manager/ui/fragment/SettingsFragment.java
+++ app/src/main/java/org/lsposed/manager/ui/fragment/SettingsFragment.java
@@ -185,24 +185,24 @@ public class SettingsFragment extends BaseFragment {
                 });
             }
 
-            //Preference shortcut = findPreference("add_shortcut");
-            //if (shortcut != null) {
-            //    shortcut.setVisible(App.isParasitic);
-            //    if (!ShortcutUtil.isRequestPinShortcutSupported(requireContext())) {
-            //        shortcut.setEnabled(false);
-            //        shortcut.setSummary(R.string.settings_unsupported_pin_shortcut_summary);
-            //    }
-            //    shortcut.setOnPreferenceClickListener(preference -> {
-            //        if (!ShortcutUtil.requestPinLaunchShortcut(() -> {
-            //            setNotificationPreferenceEnabled(notificationPreference, true);
-            //            App.getPreferences().edit().putBoolean("never_show_welcome", true).apply();
-            //            parentFragment.showHint(R.string.settings_shortcut_pinned_hint, false);
-            //        })) {
-            //            parentFragment.showHint(R.string.settings_unsupported_pin_shortcut_summary, true);
-            //        }
-            //        return true;
-            //    });
-            //}
+            Preference shortcut = findPreference("add_shortcut");
+            if (shortcut != null) {
+                shortcut.setVisible(App.isParasitic);
+                if (!ShortcutUtil.isRequestPinShortcutSupported(requireContext())) {
+                    shortcut.setEnabled(false);
+                    shortcut.setSummary(R.string.settings_unsupported_pin_shortcut_summary);
+                }
+                shortcut.setOnPreferenceClickListener(preference -> {
+                    if (!ShortcutUtil.requestPinLaunchShortcut(() -> {
+                        setNotificationPreferenceEnabled(notificationPreference, true);
+                        App.getPreferences().edit().putBoolean("never_show_welcome", true).apply();
+                        parentFragment.showHint(R.string.settings_shortcut_pinned_hint, false);
+                    })) {
+                        parentFragment.showHint(R.string.settings_unsupported_pin_shortcut_summary, true);
+                    }
+                    return true;
+                });
+            }
 
             Preference backup = findPreference("backup");
             if (backup != null) {
